07346df5dbf0730d2b59d1c9e3a124d285bbf7ed
LAPTOP-QREC9ALC\atulshakya <ashaky20@student.scad.edu>
2016-10-09 23:34:22 +0545

    Movement Script Updated

---
+++ Unity Files/Dark Before Dawn/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	Rigidbody rigidBody;
+	float moveSpeed;
+	float jumpHeight;
+	Vector3 playerMove;
+
+	bool isGrounded = false;
+	bool inAir = false;
+	public bool doubleJump = false;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = transform.GetComponent<Rigidbody> ();
+		moveSpeed = 3f;
+		jumpHeight = 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		rigidBody.freezeRotation = true;
+
+		if (Input.GetButtonDown ("Vertical") && isGrounded) {
+			Debug.Log ("Up");
+			rigidBody.AddForce (new Vector3 (0f, jumpHeight, 0), ForceMode.Impulse);
+			inAir = true;
+		} else if (Input.GetButtonDown ("Vertical") && inAir && doubleJump) {
+			rigidBody.AddForce (new Vector3 (0f, jumpHeight, 0), ForceMode.Impulse);
+			inAir = false;
+		}
+	}
+
+	void FixedUpdate(){
+
+		playerMove.Set (Input.GetAxis ("Horizontal"), 0,0);
+
+		Vector3 newPostion = transform.position + playerMove.normalized * moveSpeed * Time.deltaTime;
+
+		rigidBody.MovePosition (newPostion);
+
+	}
+
+	void OnCollisionEnter (Collision col){
+		if (col.collider.tag == "Ground") {
+			Debug.Log ("Grounded");
+			isGrounded = true;
+		}
+	}
+
+	void OnCollisionExit(Collision col){
+		isGrounded = false;
+	}
+}
