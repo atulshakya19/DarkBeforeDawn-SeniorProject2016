6a1c26aaa65856d9015bd35a1ffeb64f5ee903b7
IZZY\Fernando <falcan20@student.scad.edu>
2016-10-18 14:58:05 -0400

    Added a new Scene for the Start Screen, also made a new script for buttons, and started to work on the health bar.

---
+++ Unity Files/Dark Before Dawn/Assets/Scripts/Buttons.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	public void HandleButton(string button_name){
+		switch (button_name) {
+		case "level 1":
+			Application.LoadLevel ("Player Controls");
+			break;
+		}
+	}
+
+}
