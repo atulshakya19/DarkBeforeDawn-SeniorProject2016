00702ca02ec1416257325d176a3a581eb059279c
LAPTOP-QREC9ALC\memys <shakya.atul419@gmail.com>
2016-10-18 06:42:12 +0545

    Enemy Chase Script Updated

---
+++ Unity Files/Dark Before Dawn/Assets/Scripts/Alien.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alien : MonoBehaviour {
+
+	public int attackValue;
+	public int health;
+	public float speed = 0.10f;
+
+	public static int numAlien;
+	public bool die;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		numAlien++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = (player.transform.position - transform.position).normalized;
+		float distance = (player.transform.position - transform.position).magnitude;
+		Vector3 move = transform.position + (direction * speed);
+		transform.position = move;
+		if (distance < 1f) {
+			die = true;
+		}
+		if (die) {
+			numAlien--;
+			Destroy (gameObject);
+		}
+	}
+}
--- Unity Files/Dark Before Dawn/Assets/Scripts/MovementAI.cs
+++ Unity Files/Dark Before Dawn/Assets/Scripts/MovementAI.cs
@@ -1,19 +1,23 @@
-﻿using UnityEngine;
+﻿ using UnityEngine;
 using System.Collections;
 
 public class MovementAI : MonoBehaviour {
 
 	private Rigidbody rigidbody;
-	public float AISpeed;
+	public float AISpeed = 2;
+	public float speed = 0.10f;
+	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
 		rigidbody = GetComponent<Rigidbody> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate (new Vector3 (AISpeed, 0, 0) * Time.deltaTime); 
+		rigidbody.freezeRotation = true;
 	}
 
 	void OnTriggerEnter (Collider other){
@@ -25,6 +29,13 @@ public class MovementAI : MonoBehaviour {
 	void OnCollisionEnter (Collision col){
 		if (col.transform.tag == "Player") {
 			Destroy (gameObject);
-		}
+		} 
+	}
+		
+	public void ChasePlayer(){
+		Vector3 direction = (player.transform.position - transform.position).normalized;
+		float distance = (player.transform.position - transform.position).magnitude;
+		Vector3 move = transform.position + (direction * speed/2);
+		transform.position = move;
 	}
 }
