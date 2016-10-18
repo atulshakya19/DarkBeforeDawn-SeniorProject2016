3fef5e62133a18e8990a6c08c5c6e510a4270c5d
IZZY\Fernando <falcan20@student.scad.edu>
2016-10-18 16:00:16 -0400

    Fixed level button

--- Unity Files/Dark Before Dawn/Assets/Scripts/Buttons.cs
+++ Unity Files/Dark Before Dawn/Assets/Scripts/Buttons.cs
@@ -5,11 +5,7 @@ using System.Collections;
 public class Buttons : MonoBehaviour {
 
 	public void HandleButton(string button_name){
-		switch (button_name) {
-		case "level 1":
-			Application.LoadLevel ("Player Controls");
-			break;
+		Application.LoadLevel (1);
 		}
-	}
-
 }
+	
