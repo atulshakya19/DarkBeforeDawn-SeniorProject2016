fb2d3011ae07d60b25bcde088dbc6d402b985548
IZZY\Fernando <falcan20@student.scad.edu>
2016-10-20 14:19:18 -0400

    Added Player Health, and start screen

---
+++ Unity Files/Dark Before Dawn/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int fullHealth = 100;
+	public int currentHealth;
+	public Slider healthBar;
+
+	bool damaged;
+	bool isDead;
+
+	// Use this for initialization
+	void Awake () {
+
+		currentHealth = fullHealth;	
+
+	}
+	
+	// Update is called once per frame
+	void Update (){
+	
+	}
+
+	public void TakeDamage(int amount){
+
+		damaged = true;
+
+		currentHealth -= amount;
+
+		healthBar.value = currentHealth;
+
+		if (currentHealth <= 0 && !isDead) {
+			Death ();
+		}
+
+	}
+
+	void Death(){
+
+		isDead = true;
+
+	}
+}
