c7791b7f001a40a363f0c10a172b208f9d8b2f3a
LAPTOP-QREC9ALC\memys <shakya.atul419@gmail.com>
2016-10-30 02:44:18 +0545

    Teleport and Gem change

---
+++ Unity Files/Dark Before Dawn/Assets/Scripts/Player/ArrayExample.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrayExample : MonoBehaviour {
+
+	public int[] Scores =  new int[10];
+	public string[] Names = new string[10];
+
+	public class ArrayClass
+	{
+	}
+	//this one will not show up in the inspector panel
+	public ArrayClass[] MyArray= new ArrayClass[10];
+
+	public GameObject[] MyGameObjects;
+
+	public int ArrayLength;
+
+	// we can set the values in the array upon declaration
+	public int[] Primes = new int[]{1,3,5,7,9};
+
+	// Use this for initialization
+	void Start () 
+	{
+		Debug.Log (MyGameObjects.Length);
+
+		for (int i = 0; i < MyGameObjects.Length; i++)
+		{
+			MyGameObjects[i].name = i.ToString();
+		}
+		foreach (GameObject go in MyGameObjects) 
+		{
+			Debug.Log (go.name);
+		}
+		//dynamic initialization
+		float[] DynamicFloats = new float[ArrayLength];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity Files/Dark Before Dawn/Assets/Scripts/Portal/CreateRift.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateRift : MonoBehaviour {
+
+	private Vector2 _destiny;
+	public GameObject rift;
+
+	private GemPickup _gemActive;
+	private Rigidbody _rigidbody;
+	private MovementScript _move;
+	private GameObject riftA;
+
+	public bool _enterRift;
+	public bool _riftThere;
+
+	// Use this for initialization
+	void Start () {
+	
+		_move = GetComponent<MovementScript>();
+		_rigidbody = GetComponent<Rigidbody> ();
+		_gemActive = GetComponent <GemPickup> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		_destiny = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		string GemActive = _gemActive.ActiveGem;
+
+		if (GemActive != "Gem 2") {
+			Destroy (riftA);
+			_riftThere = false;
+			_rigidbody.isKinematic = false;
+			_move.enabled = true;
+			_enterRift = false;
+		}
+
+		if (!_riftThere && GemActive == "Gem 2") {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				riftA = (GameObject)Instantiate (rift, _destiny, Quaternion.identity);
+				_riftThere = true;
+			}
+		} else if (_enterRift && GemActive == "Gem 2" ) {
+			Destroy (riftA);
+			_move.enabled = false;
+			_rigidbody.isKinematic = true;
+
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				transform.position = _destiny;
+				_move.enabled = true;
+				_enterRift = false;
+				_riftThere = false;
+				_rigidbody.isKinematic = false;
+			}
+		} else if (_riftThere && GemActive == "Gem 2" ) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				Destroy (riftA);
+				_riftThere = false;
+			}
+		}
+	}
+
+
+	void OnTriggerEnter (Collider other){
+		if (other.transform.tag == "Rift") {
+			_enterRift = true;	
+		}
+	}
+}
