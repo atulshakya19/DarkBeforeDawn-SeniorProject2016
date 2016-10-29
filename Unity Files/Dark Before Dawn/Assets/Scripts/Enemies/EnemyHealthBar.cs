6f879d1c24e224fe0bea96dc3bd25105dda0c258
IZZY\Fernando <falcan20@student.scad.edu>
2016-10-29 16:55:59 -0400

    Made folders

---
+++ Unity Files/Dark Before Dawn/Assets/Scripts/Enemies/EnemyHealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealthBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
