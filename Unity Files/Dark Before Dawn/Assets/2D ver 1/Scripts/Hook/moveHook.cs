e6e0e4887f25d6b013067e1fa361798752f21085
DESKTOP-N02PO6I\atulshakya <shakya.atul419@gmail.com>
2016-11-10 03:49:57 -0500

    Atul's Part and Rope

---
+++ Unity Files/Dark Before Dawn/Assets/2D ver 1/Scripts/Hook/moveHook.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveHook : MonoBehaviour {
+
+	private HookFire _hook;
+	private Vector2 _direction;
+	private bool _touch = false;
+
+	// Use this for initialization
+	void Start () {
+		_hook = GameObject.FindObjectOfType<HookFire> ();
+		_direction.x = _hook.direction.x;
+		_direction.y = _hook.direction.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!_touch) {
+
+			Move ();
+		}
+
+	}
+
+	void Move (){
+
+		transform.Translate (_direction.normalized * Time.deltaTime *25f);
+	}
+		
+
+	void OnTriggerEnter2D (Collider2D col){
+		Debug.Log ("Hi1");
+		if (col.gameObject.tag == "Swinging Platform") {
+			Debug.Log ("Hit");
+			_touch = true;
+			//destiny = transform.position;
+		}
+	}
+
+}
